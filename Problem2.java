2ad7298e0bda7b96007c1663ac1864215b8ed86d
Fatimaalhajri <fatimaalhajri980@gmail.com>
2024-07-20 23:07:28 +0400

    Problem 2 completed

--- Problem2.java
+++ Problem2.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-
+import java.util.Stack;
+// Problem 2: Convert Decimal to Binary
 public class Problem2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -14,9 +15,34 @@ public class Problem2 {
             return;
         }
 
-        //String binaryNumber = convertDecimalToBinary(decimalNumber);
+        String binaryNumber = convertDecimalToBinary(decimalNumber);
 
-        //System.out.println("Output: "+ binaryNumber);
+        System.out.println("Output: "+ binaryNumber);
     }
+    // to convert a positive decimal number to its binary equivalent
+    public static String convertDecimalToBinary(int decimalNumber) {
+        // if the number is 0, return 0
+        if (decimalNumber == 0) {
+            return "0";
+        }
 
+        Stack<Integer> stack = new Stack<>();
+
+        // if number greater than 0
+        while (decimalNumber > 0) {
+            int remainder = decimalNumber % 2;
+            stack.push(remainder);
+            decimalNumber = decimalNumber / 2;
+        }
+
+        // Create a string to store the binary equivalent
+        StringBuilder binaryNumber = new StringBuilder();
+
+        // Pop the stack to get the binary equivalent
+        while (!stack.isEmpty()) {
+            binaryNumber.append(stack.pop());
+        }
+
+        return binaryNumber.toString();
+    }
 }
